0f98de07eddbb3215f3c421f41b2a77430423bf2
Yin-Jui <ckjimmy634@gmail.com>
2019-07-17 15:00:29 +0800

    number guessing game is available

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,13 +2,44 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    val se = SecretNumber()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "secret: " + se.secret)
 
+    }
 
+    fun check(view: View) {
+        val n: Int = input_num.text.toString().toInt()
+
+        Log.d("MainActivity", "number: " + n)
+
+        val diff = se.validate(n)
+        var message = "Bingo"
+
+        if (diff < 0) {
+
+            message = "Bigger"
+        } else if (diff > 0) {
+            message = "Smaller"
+        }
+//        Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+
+        AlertDialog.Builder(this)
+            .setTitle("Message")
+            .setMessage(message)
+            .setPositiveButton("OK",null)
+            .show()
     }
 }
---
+++ app/src/main/java/com/example/myapplication/SecretNumber.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import java.util.*
+
+class SecretNumber {
+
+    val secret: Int = Random().nextInt(10) + 1
+
+    var count = 0
+
+    fun validate(number: Int): Int {
+
+        count++
+        return number - secret
+
+    }
+}
+
+    fun main(){
+
+        val secretNumber = SecretNumber()
+        println(secretNumber.secret)
+        println(secretNumber.validate(2))
+    }
